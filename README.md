9e52e8b52d32353087adafc319a02ce922ce25ec
icetbr <icetbr@yahoo.com.br>
2025-12-22 23:41:17 -0300

    v4

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Google Cleaner
+Old style search results for easier title scanning and faster access to common search filters.
 
-- old style links
-- moves the top bar (All, Videos, News...) to the sidebar
-- hides "rich search content" (Top stories...)
-- adds toggle results in english/all languages
+New in v4: you need to [set the default search tab to Web][5].
 
-Get as an [userscript][1] or an [firefox][2] or [chrome][3] extension.
+### Before
+![Before](media/before.png)
 
-![Before](media/googleCleanerBefore.png)
-![After](media/googleCleanerAfter.png)
+### After
+![After](media/after.png)
 
-[1]: https://openuserjs.org/users/icetbr/scripts
-[4]: https://github.com/fregante/browser-extension-template
 
+## Install
+[Userscript][1] - [Firefox][2] - [Chrome][3].
+
+
+## Recommended
+[uBlacklist][4] is a life changer, it prevents the sites you specify from appearing in the search results
+
+
+[Contributing](https://github.com/icetbr/my-projects/blob/main/CONTRIBUTING.md)\
+[License (MIT)](https://choosealicense.com/licenses/mit/)
+
+[1]: https://openuserjs.org/scripts/icetbr/Google_Cleaner
+[2]: https://addons.mozilla.org/en-US/firefox/addon/google-cleaner
+[3]: https://chrome.google.com/webstore/detail/google-cleaner/eipbokcgljcghpakakmjinohkloboifg
+[4]: https://addons.mozilla.org/en-US/firefox/addon/ublacklist/
+[5]: https://www.reddit.com/r/firefox/comments/1ctk95k/extension_to_force_google_search_to_default_to/
---
+++ src/content.js
@@ -0,0 +1,102 @@
+import { $, $$, toggle, addCss, h } from '@icetbr/utils/Web';
+const { div, button, a, span } = h;
+
+const
+    css = `
+        #gc-links                   { display: flex; flex-direction: column; gap: 8px; align-items: center; font-size: 11px; position: absolute; top: 95px; left: 37px; z-index: 999 }
+        #gc-links :is(a, button)    { all: unset; cursor: pointer; &:hover { text-decoration: underline }}
+        #gc-links > :first-child    { padding-bottom: 4px; }
+        #gc-filtersbar              { display: flex; flex-direction: column; gap: 6px; margin-left: 18px; }
+
+        .logo                       { left: -193px;                       } /* align logo with sidebar */
+        .zLSRge                     { border: none !important;            } /* grey line bellow navigation bar */
+        #taw                        { position: relative; z-index: 1;     } /* prevents "did you mean" from being cropped */
+
+
+        .MjjYud                     { margin-top: -30px;                  } /* spacing between search results */
+        .yuRUbf                     { line-height: 0.58;                  }
+
+        .B6fmyf.Mg1HEd              { display: none;                      } /* three dots menu in each link */
+        a[jsname='UWckNb'] > div    { position: inherit;                  } /* link bellow title */
+
+        .tjvcx                      { color: green; font-size: 14px;      } /* link */
+        .H9lube, .VuuXrf, .DDKf1c   { display: none;                      } /* hide link icon and title */
+    `,
+
+    toggleTopbar = () => $$('#top_nav, #appbar, .rfiSsc, .caNvfd').forEach(toggle),
+
+    toggleFiltersbar = () => toggle($('#gc-filtersbar')),
+
+    addLinks = () => {
+        const
+            toggleEnglishOnly = () => {
+                const p = new URLSearchParams(location.search)
+                p.has('gl') ? p.delete('gl') : p.set('gl', 'us')
+                return `?${p}`
+            },
+            doLink = tbsParameter => {
+                const p = new URLSearchParams(location.search)
+                p.set('tbs', tbsParameter)
+                return `?${p}`
+            },
+            showPast3YearsPosts = () => {
+                const d = new Date()
+                let minDate = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear() - 2}`
+                return doLink(`cdr:1,cd_min:${minDate},cd_max:`)
+            },
+            prependLink = qParameter => {
+                const p = new URLSearchParams(location.search)
+                const query =
+                    p.get('q')
+                    .replace(/\s*site:.*\s*/, '')
+                    .concat(' ' + qParameter)
+
+                p.set('q', query)
+                return `?${p}`
+            }
+
+            document.body.append(
+                div({id:'gc-links'},
+                    button({onclick:toggleTopbar}, 'Toggle topbar'),
+                    a({href:toggleEnglishOnly()}, 'Toggle english only'),
+                    a({href:doLink('qdr:y')}, 'Past year'),
+                    a({href:showPast3YearsPosts()}, 'Past 3 years'),
+                    a({href:prependLink('site:news.ycombinator.com')}, 'Hackernews'),
+                    a({href:prependLink('site:reddit.com')}, 'Reddit'),
+                    button({onclick:toggleFiltersbar}, '+'),
+
+                    div({id:'gc-filtersbar'},
+                        a({href:doLink('qdr:')}, 'Any time'),
+                        a({href:doLink('qdr:h')}, 'Past hour'),
+                        a({href:doLink('qdr:d')}, 'Past 24 hours'),
+                        a({href:doLink('qdr:w')}, 'Past week'),
+                        a({href:doLink('qdr:m')}, 'Past month'),
+                    )
+                )
+            )
+    },
+
+    showRealUrls = () => {
+        $$('cite').forEach(e => {
+            const url = (e.textContent.startsWith('http') ? e.textContent : e.closest('a')?.href || '').replace('https://', '').replace('www.', '')
+            if (!url) return;
+            const breadcrumb = span({className: "ylgVCe ob9lvb", role: "text"}, ` › ${e.textContent}`)
+
+            const newNodes =
+                e.textContent.startsWith('http')    ? [url] :
+                url.startsWith('reddit.com')        ? [url.split('/comments')[0], breadcrumb] :
+                url.startsWith('youtube.com')       ? ['youtube.com', breadcrumb] :
+                url.startsWith('stackoverflow.com') ? ['stackoverflow.com', breadcrumb] :
+                                                      [url, breadcrumb]
+            e.textContent = ''
+            newNodes.forEach(n => e.append(n)) // needs to append one at a time because otherwise the DomNode is casted to a string
+        })
+    }
+
+if (!document.title.includes('Google Shopping')) {
+    addCss(css)
+    addLinks()
+    toggleTopbar()
+    toggleFiltersbar()
+    showRealUrls()
+}
